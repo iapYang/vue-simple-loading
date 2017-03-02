5a7fd8ea29e40463b0fa7746ba7b80411930f3b2
iapYang <yklorse@gmail.com>
2017-03-02 11:43:09 +0800

    0302 auto add port

--- conf/webpack.dev.config.js
+++ conf/webpack.dev.config.js
@@ -1,17 +1,13 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
-const findPortSync = require('find-port-sync');
 
 const baseWebpackConfig = require('./webpack.base.config.js');
-
-const port = findPortSync();
 
 module.exports = merge(baseWebpackConfig, {
     devtool: 'eval-source-map',
     devServer: {
         contentBase: './dev',
         host: 'localhost',
-        port,
         open: true,
         hot: true,
     },
