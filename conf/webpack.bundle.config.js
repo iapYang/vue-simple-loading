69cdb05e36eeadb2a11f2fc64a296d3e62f120af
iapYang <yklorse@gmail.com>
2017-03-28 11:19:21 +0800

    build success

--- conf/webpack.bundle.config.js
+++ conf/webpack.bundle.config.js
@@ -2,21 +2,13 @@ const path = require('path');
 const glob = require('glob');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-
 const baseWebpackConfig = require('./webpack.base.config.js');
 
-const jsFiles = glob.sync('./dev/component/src/*.js');
-
-const entry = {};
-jsFiles.forEach((file, i) => {
-    entry[path.basename(file, '.js')] = file;
-});
-
 module.exports = merge(baseWebpackConfig, {
-    entry,
+    entry: path.resolve(__dirname, '../dev/component/src/index.js'),
     output: {
         path: path.join(process.cwd(), 'bundle'),
-        filename: '[name].min.js',
+        filename: 'index.js',
         libraryTarget: 'umd',
     },
     plugins: [
--- dev/component/src/VueLoading.vue
+++ dev/component/src/VueLoading.vue
@@ -36,6 +36,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import '../../style/reset';
     .vue-loading-container {
         position: relative;
         display: inline-block;
